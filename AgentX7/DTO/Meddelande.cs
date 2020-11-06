ba730fbf0b6769feaf5bd69516d7d323d5406754
Jonas Sjöhag <jonas.sjohag@dekiru.se>
2020-11-06 10:31:15 +0100

    DTO

---
+++ AgentX7/DTO/Meddelande.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgentX7.DTO
+{
+    public class Meddelande
+    {
+        public int Id { get; set; }
+        public string Spyhandle { get; set; }
+        public string Binary { get; set; }
+    }
+}
